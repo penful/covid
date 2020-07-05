bd8610ee23f78f92e2e433faef355b96870f81dd
Fulvia <fulvia.pennoni@unimib.it>
2020-07-05 12:52:58 +0200

    Coefficients divided by 100
    Coefficients divided by 100

--- Result_Covid_IT_UM.R
+++ Result_Covid_IT_UM.R
@@ -8,6 +8,10 @@
 ##
 rm(list = ls())
 load("Umbria_Italy_model_now_positive.Rdata")
+require(tscount)
+#### 
+summary(totUmbria)
+summary(totItaly)
 #### TABLE 1 ####
 AIC1Umbria <- rbind(A1Umbria,A2Umbria,A3Umbria,A4Umbria)
 AIC1Umbria
@@ -22,6 +26,8 @@ summary(M1Umbria)[5]
 summary(M2Umbria)[5]
 summary(M3Umbria)[5]
 summary(M4Umbria)[5]
+round(M1Umbria$coefficients,3)
+round(M2Umbria$coefficients,3)
 round(M3Umbria$coefficients,3)
 round(M4Umbria$coefficients,3)
 #
@@ -29,8 +35,10 @@ summary(M1Italy)[5]
 summary(M2Italy)[5]
 summary(M3Italy)[5]
 summary(M4Italy)[5]
-round(M3Italy$coefficients,3)
-round(M4Italy$coefficients,3)
+round(M1Italy$coefficients,6)
+round(M2Italy$coefficients,6)
+round(M3Italy$coefficients,6)
+round(M4Italy$coefficients,6)
 
 #### overdispesion s.e.####
 summary(M4Italy)
@@ -47,7 +55,8 @@ cbind((P4Italy$interval[,1]),(P4Italy$interval[,2]))
 #### FIGURE 1 UMBRIA  ####
 n<-dim(dataUmbria)[1]
 m<-max(totUmbria)
-ytick<-c(0,500,1000,1500)
+#ytick<-c(0,500,950)
+ytick<-c(0,250,500,750,1000)
 xtick<-c(1,50,100,133)
 
 plot(totUmbria, 
@@ -57,7 +66,7 @@ plot(totUmbria,
       yaxt="n",
      xaxt="n",
      xlim =c(1,n+5),
-     ylim = c(0,m+5),
+     ylim = c(0,1000),
      lwd = 0.5, 
      lty = 1,col ="black" )
 abline(v=128, col = "grey")
@@ -92,7 +101,7 @@ categories<-c(
  expression(italic(M)[4]))
 col = c(1,2,3,4,1)
 legend( 
-  98,1500,
+  98,900,
   pch = c(20,NA,NA,NA,NA),
   lty = c(NA,2,3,4,1),
   legend = categories, 
@@ -106,7 +115,8 @@ legend(
 #### FIGURE 2 ITALY ####
 n<-dim(dataUmbria)[1]
 m<-max(totItaly)
-ytick<-c(200,27009,54018,81027,110000)
+#ytick<-c(0,27009,54018,81027,110000)
+ytick<-c(0,30000,60000,90000,120000)
 xtick<-c(1,50,100,133)
 plot(totItaly, 
    line = 2.5,
@@ -115,7 +125,7 @@ plot(totItaly,
        yaxt="n",
      xaxt="n",
      xlim =c(1,n+5),
-     ylim = c(0,m+5),
+     ylim = c(0,120000),
      lwd = 0.5, 
      lty = 1,col ="black" )
 abline(v=128, col = "gray")
@@ -141,7 +151,7 @@ lines(c(M3Italy$fitted.values,P3Italy$pred),
       lwd=1, col = 4, lty = 4)
 lines(c(M4Italy$fitted.values,P4Italy$pred), 
       lwd=1, col = 1, lty = 1)
-legend(95, 109000, 
+legend(98, 108000, 
   pch = c(20,NA,NA,NA,NA),
   lty = c(NA,2,3,4,1),
   legend = categories, 
@@ -159,8 +169,9 @@ prevIT<-M4IT/60317000
 n<-length(prevIT)
 # times 1000
 plot(prevIT[1:133]*1000, 
-     ylab= "Prevalenza dei casi attualmente positivi",  
+     ylab= "Prevalenza dei casi attualmente positivi (x1000)",  
      line = 2.5,
+     ylim = c(0,2),
      col = "blue4",
      xlab=expression("Giorni ("*italic(t)*")"),
      yaxt="n",
@@ -170,14 +181,17 @@ plot(prevIT[1:133]*1000,
      lty = 1)
 lines(prevUM[1:133]*1000, 
       lwd=1, col = "red2", lty = 1)
-ytick<-c(0,0.4, 0.9, 1.3,1.8)
+#ytick<-c(0,0.4, 0.9, 1.3,1.8)
+ytick<-c(0, 0.5, 1.0, 1.5, 2)
 xtick<-c(1,50,100,133)
 abline(v=128, col = "gray")
+
 axis(side=2, at=ytick, labels = FALSE, 
      cex.lab = 0.5,tck=-0.009)
 text(par("usr")[1], 
      ytick,  
-     labels = ytick, srt = 45, 
+     labels = c("0.0", "0.5", "1.0","1.5", "2.0"), 
+     srt = 45, 
      pos = 2, xpd = TRUE, cex.lab = 0.5)
 
 axis(side=1, at=xtick, labels = FALSE, cex=0.5,tck=-0.009)
@@ -190,7 +204,7 @@ text(x=xtick,
 categories<-c(expression("Umbria"),
               expression("Italia"))
 col = c("red2","blue4")
-legend(95, 1.8,
+legend(98, 1.8,
        pch = c(NA,NA),
        lty = c(1,1),
        legend = categories, 
--- Umbria_and_Italy_data.R
+++ Umbria_and_Italy_data.R
@@ -6,20 +6,22 @@ overall.dataset <- "dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale
 overall.filename<-paste(repository,overall.dataset,sep="")
 #### all categories ####
 Italy<-read.csv(overall.filename)
-#### Select 5 categories ####
+#### only the 5 selected categories ####
 names(Italy)
 library(dplyr)
 dataItaly<- Italy %>%
  select(dimessi_guariti,isolamento_domiciliare,
  ricoverati_con_sintomi,
  terapia_intensiva,deceduti)
+
 dataItaly<-dataItaly[1:128,]
+#save(dataItaly, file = "dataItaly.Rdata")
 
 #### DATA UMBRIA ####
 regional.dataset <- "dati-regioni/dpc-covid19-ita-regioni.csv"
-regional.filename<-paste(repository,regional.dataset,
-                         sep="")
+regional.filename<-paste(repository,regional.dataset,sep="")
 regions<-read.csv(regional.filename)
+
 myregion<-"Umbria"
 myDateTimeStr1 <-regions$data[regions$denominazione_regione==myregion]
 myPOSIXct1 <- as.POSIXct(myDateTimeStr1, format="%Y-%m-%dT %H:%M:%S")
@@ -27,7 +29,8 @@ days <- as.Date(myPOSIXct1)
 step.ahead<-1
 days.ahead <- seq(days[length(days)]+1,days[length(days)]+step.ahead,by=1)
 require(xts)
-select <- "dimessi_guariti"
+
+select = "dimessi_guariti"
 y <- as.numeric(unlist(subset(regions,
                               subset = (denominazione_regione == myregion),
                               select = select)))
@@ -39,26 +42,26 @@ y <- as.numeric(unlist(subset(regions,
                               select = select)))
 count2<-xts(c(y,rep(NA,step.ahead)), order.by = c(days,days.ahead), frequency = 7)
 #
-select <- "ricoverati_con_sintomi"
+select = "ricoverati_con_sintomi"
 y <- as.numeric(unlist(subset(regions,
                               subset = (denominazione_regione == myregion),
                               select = select)))
 count3<-xts(c(y,rep(NA,step.ahead)), order.by = c(days,days.ahead), frequency = 7)
 #
-select <- "terapia_intensiva"
+select = "terapia_intensiva"
 y <- as.numeric(unlist(subset(regions,
                               subset = (denominazione_regione == myregion),
                               select = select)))
 count4<-xts(c(y,rep(NA,step.ahead)), 
              order.by = c(days,days.ahead), frequency = 7)
 #
-select <- "deceduti"
+select = "deceduti"
 y <- as.numeric(unlist(subset(regions,
                               subset = (denominazione_regione == myregion),
                               select = select)))
 count5<-xts(c(y,rep(NA,step.ahead)), order.by = c(days,days.ahead), frequency = 7)
 #
-dataUmbria<-cbind(count1,count2,count2,count4,count5)
+dataUmbria<-cbind(count1,count2,count3,count4,count5)
 n<-dim(dataUmbria)[1]; n
 dataUmbria<-data.frame(dataUmbria[-n,])
 tail(dataUmbria)
@@ -68,3 +71,4 @@ names(dataUmbria)<-c("dimessi_guariti",
                  "terapia_intensiva",
                  "deceduti")
 dataUmbria<-dataUmbria[1:128,]
+#save(dataUmbria, file = "dataUmbria.Rdata")
